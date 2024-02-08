6525c0f0a7abfbb46fbec5682c3e6192cd99ee86
Croquignolex <gpetitalex10@gmail.com>
2024-02-08 13:37:50 +0100

    Add project files

---
+++ croquy-shop-api-spring-java/src/main/java/com/shop/croquy/CroquyShopApiSpringJavaApplication.java
@@ -0,0 +1,13 @@
+package com.shop.croquy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CroquyShopApiSpringJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CroquyShopApiSpringJavaApplication.class, args);
+    }
+
+}
