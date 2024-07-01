156265f24a978628d3e3d608d4090cd81cdc781a
Croquignolex <gpetitalex10@gmail.com>
2024-07-01 05:14:55 +0100

    Update data

--- src/main/java/com/shop/croquy/CroquyShopApiSpringJavaApplication.java
+++ src/main/java/com/shop/croquy/CroquyShopApiSpringJavaApplication.java
@@ -7,6 +7,10 @@ import com.shop.croquy.v1.repositories.CountryRepository;
 import com.shop.croquy.v1.repositories.StateRepository;
 import com.shop.croquy.v1.repositories.BrandRepository;
 import com.shop.croquy.v1.repositories.ShopRepository;
+import com.shop.croquy.v1.repositories.GroupRepository;
+import com.shop.croquy.v1.repositories.CategoryRepository;
+import com.shop.croquy.v1.repositories.VendorRepository;
+import com.shop.croquy.v1.repositories.AttributeRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -29,6 +33,10 @@ public class CroquyShopApiSpringJavaApplication {
     private final StateRepository stateRepository;
     private final BrandRepository brandRepository;
     private final ShopRepository shopRepository;
+    private final GroupRepository groupRepository;
+    private final CategoryRepository categoryRepository;
+    private final VendorRepository vendorRepository;
+    private final AttributeRepository attributeRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(CroquyShopApiSpringJavaApplication.class, args);
@@ -44,7 +52,7 @@ public class CroquyShopApiSpringJavaApplication {
         return (String[] args) -> {
             if(userRepository.findByUsername("user100").isEmpty()) {
                 List<User> users = new ArrayList<>();
-                List<Shop> shops = new ArrayList<>();
+                List<Attribute> attributes = new ArrayList<>();
 
                 User user = userRepository.save(seedUsers(200, Role.CUSTOMER));
 
@@ -61,21 +69,48 @@ public class CroquyShopApiSpringJavaApplication {
                 state.setDescription("This description is a well description for this state. Make sur you figure this well");
 
                 Brand brand = new Brand();
-                brand.setName("Brand");
-                brand.setSlug("brand");
-                brand.setWebsite("brand@exemple.com");
-                brand.setSeoTitle("brand");
+                brand.setName("Nike");
+                brand.setSlug("nike");
+                brand.setWebsite("info@nike.com");
+                brand.setSeoTitle("Nike");
                 brand.setSeoDescription("This seo description is a well description for this brand. Make sur you figure this well");
                 brand.setCreator(user);
                 brand.setDescription("This description is a well description for this brand. Make sur you figure this well");
 
+                Shop shop = new Shop();
+                shop.setName("Siège");
+                shop.setSlug("siege");
+                shop.setCreator(user);
+                shop.setDescription("This description is a well description for this shop. Make sur you figure this well");
+
+                Group group = new Group();
+                group.setName("Appliances");
+                group.setSlug("appliances");
+                group.setSeoTitle("Appliances");
+                group.setSeoDescription("This seo description is a well description for this group. Make sur you figure this well");
+                group.setCreator(user);
+                group.setDescription("This description is a well description for this group. Make sur you figure this well");
+
+                Category category = new Category();
+                category.setName("Kitchen");
+                category.setSlug("kitchen");
+                category.setSeoTitle("Kitchen");
+                category.setSeoDescription("This seo description is a well description for this category. Make sur you figure this well");
+                category.setCreator(user);
+                category.setGroup(group);
+                category.setDescription("This description is a well description for this category. Make sur you figure this well");
+
+                Vendor vendor = new Vendor();
+                vendor.setName("Wise computer");
+                vendor.setCreator(user);
+                vendor.setDescription("This description is a well description for this vendor. Make sur you figure this well");
+
                 for(int i = 0; i < 25; i++){
-                    Shop shop = new Shop();
-                    shop.setName("Brand" + i);
-                    shop.setSlug("brand" + i);
-                    shop.setCreator(user);
-                    shop.setDescription("This description is a well description for this brand. Make sur you figure this well" + i);
-                    shops.add(shop);
+                    Attribute attribute = new Attribute();
+                    attribute.setName("Attribute" + i);
+                    attribute.setCreator(user);
+                    attribute.setDescription("This description is a well description for this attribute. Make sur you figure this well" + i);
+                    attributes.add(attribute);
                 }
 
                 users.add(seedUsers(100, Role.SUPER_ADMIN));
@@ -85,7 +120,11 @@ public class CroquyShopApiSpringJavaApplication {
                 countryRepository.save(country);
                 stateRepository.save(state);
                 brandRepository.save(brand);
-                shopRepository.saveAll(shops);
+                shopRepository.save(shop);
+                groupRepository.save(group);
+                categoryRepository.save(category);
+                vendorRepository.save(vendor);
+                attributeRepository.saveAll(attributes);
             }
         };
     }
--- src/main/java/com/shop/croquy/v1/entities/Vendor.java
+++ src/main/java/com/shop/croquy/v1/entities/Vendor.java
@@ -21,24 +21,11 @@ public class Vendor extends BaseEntity {
     @Column(name = "name", nullable = false, unique = true)
     private String name;
 
-    @Column(name = "description", nullable = false, columnDefinition = "TEXT")
-    private String description = "";
-
-    @Column(name = "created_at")
-    private Date createdAt = new Date();
-
-    @Column(name = "updated_at")
-    private Date updatedAt = new Date();
-
     @OneToOne(mappedBy = "vendor")
     private VendorLogo logo;
 
     @OneToOne(mappedBy = "vendor")
     private VendorAddress address;
-
-    @ManyToOne
-    @JoinColumn(name = "creator_id", nullable = false)
-    private User creator;
 
 //    @OneToMany(mappedBy = "vendor")
 //    private Set<Inventory> inventories = new HashSet<>();
